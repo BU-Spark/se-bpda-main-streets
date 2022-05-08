2b6b8e84b9d97d511dacee18731e5f88d7937921
zhunan0905 <andyz0905@outlook.com>
2022-05-07 21:07:00 -0400

    Fix the pattern of the employment board when expanded and collapsed

--- boston-main-streets-frontend/src/components/CardItem.js
+++ boston-main-streets-frontend/src/components/CardItem.js
@@ -4,7 +4,7 @@ import { Card } from "react-bootstrap";
 const CardItem = (props) => {
 
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card style={{minHeight: '80px', height: '100%'}}>
             <Card.Body>
                 <Card.Text>{props.title}</Card.Text>
                 <h4>{props.text}</h4>
