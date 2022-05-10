47958c581d3c8060f3066df60815a82bd0eff444
Austin Wang <yyaustin@bu.edu>
2022-05-10 19:09:15 -0400

    fix the baseURL

--- boston-main-streets-frontend/src/services/mapService.js
+++ boston-main-streets-frontend/src/services/mapService.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-// const prefix = (process.env.REACT_APP_ENVIRONMENT === "DEV") ? (process.env.REACT_APP_URL_BACKEND) : ""
-// const baseURL = `${prefix}/api/map`
 const baseURL = '/api/map'
 
 const getBostonBoundary = async () => {
